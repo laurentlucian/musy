d820a9fb0d7b8d3eb44ef7bbc43f949d4e608d0c
Laurent <laurent.lucian@gmail.com>
2022-06-19 20:55:30 -0700

    refactor: useInterval from @chakra-ui

--- app/routes/$id.tsx
+++ app/routes/$id.tsx
@@ -1,9 +1,9 @@
-import { Avatar, Box, Heading, HStack, Image, Input, Progress, Stack, Text, VStack } from '@chakra-ui/react';
+import { Avatar, Box, Heading, HStack, Image, Progress, Stack, Text, useInterval } from '@chakra-ui/react';
 import type { Profile as ProfileType } from '@prisma/client';
 import type { LoaderFunction } from '@remix-run/node';
 import { redirect } from '@remix-run/node';
 import { useLoaderData } from '@remix-run/react';
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useDataRefresh } from 'remix-utils';
 import Player from '~/components/Player';
 import Tile from '~/components/Tile';
@@ -12,71 +12,52 @@ import Tiles from '~/components/Tiles';
 import { getUser, updateToken } from '~/services/auth.server';
 import { spotifyApi } from '~/services/spotify.server';
 
-import SpotifyWebApi from 'spotify-web-api-node';
+// import SpotifyWebApi from 'spotify-web-api-node';
+// const Search = () => {
+//   const spotifyApi = new SpotifyWebApi({ clientId: process.env.SPOTIFY_CLIENT_ID });
+//   const [search, setSearch] = useState<string>('');
+//   const [searchResults, setSearchResults] = useState<{ title: string; artist: string; art: string }[] | undefined>([]);
+//   // const { searchTracks } = useLoaderData<{
+//   //   searchTracks: SpotifyApi.SingleTrackResponse;
+//   // }>();
 
-const useInterval = (callback: () => void, delay: number | null) => {
-  const savedCallback = useRef<() => void>(callback);
+//   // useEffect(() => {
+//   //   if (!updateToken) return;
+//   //   spotifyApi.setAccessToken(updateToken.id);
+//   // }, [updateToken]);
 
-  useEffect(() => {
-    savedCallback.current = callback;
-  }, [callback]);
+//   useEffect(() => {
+//     if (!search) return setSearchResults([]);
+//     // if (!updateToken) return;
+//     let isSearching = false;
+//     spotifyApi.searchTracks(search).then((res) => {
+//       if (isSearching) return;
+//       setSearchResults(
+//         res.body.tracks?.items.map((track) => {
+//           return { title: track.name, artist: track.artists[0].name, art: track.album.images[1].url };
+//         }),
+//       );
+//     });
+//     return (isSearching = true);
+//   }, [search]);
 
-  useEffect(() => {
-    const tick = () => {
-      savedCallback.current();
-    };
-    if (delay !== null) {
-      let id = setInterval(tick, delay);
-      return () => clearInterval(id);
-    }
-  }, [delay]);
-};
-
-const Search = () => {
-  const spotifyApi = new SpotifyWebApi({ clientId: process.env.SPOTIFY_CLIENT_ID });
-  const [search, setSearch] = useState<string>('');
-  const [searchResults, setSearchResults] = useState<{ title: string; artist: string; art: string }[] | undefined>([]);
-  // const { searchTracks } = useLoaderData<{
-  //   searchTracks: SpotifyApi.SingleTrackResponse;
-  // }>();
-
-  // useEffect(() => {
-  //   if (!updateToken) return;
-  //   spotifyApi.setAccessToken(updateToken.id);
-  // }, [updateToken]);
-
-  useEffect(() => {
-    if (!search) return setSearchResults([]);
-    // if (!updateToken) return;
-    let isSearching = false;
-    spotifyApi.searchTracks(search).then((res) => {
-      if (isSearching) return;
-      setSearchResults(
-        res.body.tracks?.items.map((track) => {
-          return { title: track.name, artist: track.artists[0].name, art: track.album.images[1].url };
-        }),
-      );
-    });
-    return (isSearching = true);
-  }, [search]);
-
-  return (
-    <Stack height={['100vh']} width={['100vw']}>
-      <Input type="search" placeholder="songs" value={search} onChange={(e) => setSearch(e.target.value)} />
-      {searchResults?.map((track) => (
-        <VStack key={track.art}>
-          <HStack>
-            <Image src={track.art} boxSize="50px" />
-            <VStack>
-              <Text>{track.title}</Text>
-              <Text>{track.artist}</Text>
-            </VStack>
-          </HStack>
-        </VStack>
-      ))}
-    </Stack>
-  );
-};
+//   return (
+//     <Stack height={['100vh']} width={['100vw']}>
+//       <Input type="search" placeholder="songs" value={search} onChange={(e) => setSearch(e.target.value)} />
+//       {searchResults?.map((track) => (
+//         <VStack key={track.art}>
+//           <HStack>
+//             <Image src={track.art} boxSize="50px" />
+//             <VStack>
+//               <Text>{track.title}</Text>
+//               <Text>{track.artist}</Text>
+//             </VStack>
+//           </HStack>
+//         </VStack>
+//       ))}
+//     </Stack>
+//   );
+// };
 
 const Profile = () => {
   const { user, playback, recent } = useLoaderData<{
