fa281b9b015207df16f6f41503d37cb457d0cee4
Laurent <laurent.lucian@gmail.com>
2022-07-14 17:43:52 -0700

    fix: prevent loss of style on prod

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,4 +1,32 @@
-import { RemixBrowser } from '@remix-run/react';
+import * as React from 'react';
 import { hydrateRoot } from 'react-dom/client';
+import { CacheProvider } from '@emotion/react';
+import { RemixBrowser } from '@remix-run/react';
 
-hydrateRoot(document, <RemixBrowser />);
+import { ClientStyleContext } from './lib/emotion/context';
+import { createEmotionCache } from './lib/emotion/createEmotionCache';
+
+interface ClientCacheProviderProps {
+  children: React.ReactNode;
+}
+
+const ClientCacheProvider = ({ children }: ClientCacheProviderProps) => {
+  const [cache, setCache] = React.useState(createEmotionCache());
+
+  const reset = () => {
+    setCache(createEmotionCache());
+  };
+
+  return (
+    <ClientStyleContext.Provider value={{ reset }}>
+      <CacheProvider value={cache}>{children}</CacheProvider>
+    </ClientStyleContext.Provider>
+  );
+};
+
+hydrateRoot(
+  document,
+  <ClientCacheProvider>
+    <RemixBrowser />
+  </ClientCacheProvider>,
+);
--- app/root.tsx
+++ app/root.tsx
@@ -15,69 +15,12 @@ import Layout from '~/components/Layout';
 import type { UserProfile } from '~/services/auth.server';
 import { authenticator } from '~/services/auth.server';
 import { ScrollRestoration } from './hooks/useScrollRestoration';
-
-export const loader: LoaderFunction = async ({ request }) => {
-  return authenticator.isAuthenticated(request);
-};
-
-const App = () => {
-  const data = useLoaderData<UserProfile>();
-
-  return (
-    <Document>
-      <ChakraProvider theme={theme}>
-        <Layout user={data}>
-          <Outlet />
-        </Layout>
-      </ChakraProvider>
-    </Document>
-  );
-};
-
-export const ErrorBoundary = ({ error }: { error: Error }) => {
-  console.error(error);
-  return (
-    <Document title="Error">
-      <ChakraProvider theme={theme}>
-        <Layout user={null}>
-          <Heading fontSize={['sm', 'md']}>Oops, unhandled error</Heading>
-          <Text fontSize="sm">Trace(for debug): {error.message}</Text>
-        </Layout>
-      </ChakraProvider>
-    </Document>
-  );
-};
-
-export const CatchBoundary = () => {
-  let caught = useCatch();
-  let message;
-  switch (caught.status) {
-    case 401:
-      message = <Text>Oops, you shouldn't be here (No access)</Text>;
-      break;
-    case 404:
-      message = <Text>Oops, you shouldn't be here (Page doesn't exist)</Text>;
-      break;
-
-    default:
-      throw new Error(caught.data || caught.statusText);
-  }
-
-  return (
-    <Document title="Error">
-      <ChakraProvider theme={theme}>
-        <Heading fontSize={['sm', 'md']}>
-          {caught.status}: {caught.statusText}
-        </Heading>
-        <Text fontSize="sm">{message}</Text>
-      </ChakraProvider>
-    </Document>
-  );
-};
+import { withEmotionCache } from '@emotion/react';
+import { useContext, useEffect } from 'react';
+import { ClientStyleContext, ServerStyleContext } from './lib/emotion/context';
 
 export const meta: MetaFunction = () => ({
   charset: 'utf-8',
-  title: 'Musy',
   viewport: 'width=device-width,initial-scale=1',
 });
 
@@ -103,13 +46,60 @@ export let links: LinksFunction = () => {
   ];
 };
 
-const Document = ({ children, title = 'Musy' }: { children: React.ReactNode; title?: string }) => {
+export const loader: LoaderFunction = async ({ request }) => {
+  return authenticator.isAuthenticated(request);
+};
+
+const App = () => {
+  const data = useLoaderData<UserProfile>();
+
+  return (
+    <Document>
+      <ChakraProvider theme={theme}>
+        <Layout user={data}>
+          <Outlet />
+        </Layout>
+      </ChakraProvider>
+    </Document>
+  );
+};
+
+type DocumentProps = {
+  children: React.ReactNode;
+  title?: string;
+};
+
+const Document = withEmotionCache(({ children, title = 'Musy' }: DocumentProps, emotionCache) => {
+  const serverStyleData = useContext(ServerStyleContext);
+  const clientStyleData = useContext(ClientStyleContext);
+
+  // Only executed on client
+  useEffect(() => {
+    // re-link sheet container
+    emotionCache.sheet.container = document.head;
+    // re-inject tags
+    const tags = emotionCache.sheet.tags;
+    emotionCache.sheet.flush();
+    tags.forEach((tag) => {
+      (emotionCache.sheet as any)._insertTag(tag);
+    });
+    // reset cache to reapply global styles
+    clientStyleData?.reset();
+  }, []);
+
   return (
     <html lang="en">
       <head>
         <Meta />
-        <title>{title}</title>
         <Links />
+        {serverStyleData?.map(({ key, ids, css }) => (
+          <style
+            key={key}
+            data-emotion={`${key} ${ids.join(' ')}`}
+            dangerouslySetInnerHTML={{ __html: css }}
+          />
+        ))}
+        <title>{title}</title>
       </head>
       <body>
         {children}
@@ -118,6 +108,50 @@ const Document = ({ children, title = 'Musy' }: { children: React.ReactNode; tit
         <LiveReload />
       </body>
     </html>
+  );
+});
+
+export const ErrorBoundary = ({ error }: { error: Error }) => {
+  console.error(error);
+  return (
+    <Document title="Musy - Error">
+      <ChakraProvider theme={theme}>
+        <Layout user={null}>
+          <Heading fontSize={['sm', 'md']}>Oops, unhandled error</Heading>
+          <Text fontSize="sm">Trace(for debug): {error.message}</Text>
+        </Layout>
+      </ChakraProvider>
+    </Document>
+  );
+};
+
+export const CatchBoundary = () => {
+  let caught = useCatch();
+  let message;
+  switch (caught.status) {
+    case 401:
+      message = <Text>Oops, you shouldn't be here (No access)</Text>;
+      break;
+    case 404:
+      message = <Text>Oops, you shouldn't be here (Page doesn't exist)</Text>;
+      break;
+
+    default:
+      // throw new Error(caught.data || caught.statusText);
+      message = <Text>Oops, this definitely shouldn't have happened</Text>;
+  }
+
+  return (
+    <Document title="Musy - Error">
+      <ChakraProvider theme={theme}>
+        <Layout user={null}>
+          <Heading fontSize={['sm', 'md']}>
+            {caught.status}: {caught.statusText}
+          </Heading>
+          <Text fontSize="sm">{message}</Text>
+        </Layout>
+      </ChakraProvider>
+    </Document>
   );
 };
 
