3f277cc07e8def5493404fc4ee29a6a785398739
Laurent <laurent.lucian@gmail.com>
2022-09-21 22:43:13 -0700

    refactor: clean

--- app/components/Following.tsx
+++ app/components/Following.tsx
@@ -1,6 +1,6 @@
-import { Button, Flex, Input } from "@chakra-ui/react";
-import type { Profile as ProfileType } from "@prisma/client";
-import { Form, useSubmit } from "@remix-run/react";
+import { Button, Flex, Input } from '@chakra-ui/react';
+import type { Profile as ProfileType } from '@prisma/client';
+import { Form, useSubmit } from '@remix-run/react';
 
 type FollowingType = {
   user: ProfileType;
@@ -8,20 +8,24 @@ type FollowingType = {
   currentUser: ProfileType;
 };
 
-const Following = ({ currentUser, user, following }): FollowingType => {
+const Following = ({ currentUser, user, following }: FollowingType) => {
   const submit = useSubmit();
 
-  const text = following[0] ? "Unfollow" : "Follow";
+  const text = following ? 'Unfollow' : 'Follow';
 
-  currentUser.userId !== user.id && (
-    <Flex mt={1} ml={-2}>
-      <Form method="post">
-        <Input type="hidden" name={text} />
-        <Button ml={"10px"} borderRadius={"md"} onClick={(e) => submit(e.currentTarget.form)}>
-          {text}
-        </Button>
-      </Form>
-    </Flex>
+  return (
+    <>
+      {currentUser.userId !== user.userId && (
+        <Flex mt={1} ml={-2}>
+          <Form method="post">
+            <Input type="hidden" name={text} />
+            <Button ml="10px" borderRadius="md" onClick={(e) => submit(e.currentTarget.form)}>
+              {text}
+            </Button>
+          </Form>
+        </Flex>
+      )}
+    </>
   );
 };
 
