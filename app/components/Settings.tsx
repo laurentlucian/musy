d001ecff48f90f226bc32e80eb669e4a33c21916
Daniel <daniel.valdecantos@gmail.com>
2022-12-31 11:34:29 -0800

    feat: update settings animation

--- app/components/Settings.tsx
+++ app/components/Settings.tsx
@@ -1,13 +1,14 @@
 import { IconButton, keyframes } from '@chakra-ui/react';
-import { Setting2 } from 'iconsax-react';
+import { RotateRight, Setting2 } from 'iconsax-react';
 import { motion } from 'framer-motion';
 import { useNavigate } from '@remix-run/react';
 
 const Settings = () => {
-  const spin = keyframes`
-  0% { transform: rotate(0deg); }
-  100% { transform: rotate(359deg); }
-`;
+  //   const spin = keyframes`
+  //   0% { transform: rotate(0deg); }
+  //   100% { transform: rotate(359deg); }
+  // `;
+  // animation: `${spin} 4s infinite linear`
   const navigate = useNavigate();
 
   return (
@@ -18,7 +19,7 @@ const Settings = () => {
       variant="ghost"
       onClick={() => navigate(`/settings`)}
       _active={{ boxShadow: 'none' }}
-      _hover={{ boxShadow: 'none', opacity: 1, animation: `${spin} 4s infinite linear` }}
+      _hover={{ boxShadow: 'none', opacity: 1, transform: 'rotate(180deg)' }}
       opacity="0.5"
       transition="opacity 2s ease in out"
       cursor="pointer"
