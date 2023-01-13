8bb67319a8942972e512458a07245c8dda8db7ae
Daniel <daniel.valdecantos@gmail.com>
2023-01-13 15:35:49 -0800

    feat: add timestamps to recommended

--- app/components/tiles/Recommended.tsx
+++ app/components/tiles/Recommended.tsx
@@ -1,11 +1,16 @@
-import { Stack } from '@chakra-ui/react';
-import type { RecommendedSongs } from '@prisma/client';
+import { Stack, Text } from '@chakra-ui/react';
+import type { Profile, RecommendedSongs } from '@prisma/client';
 import Tile from '../Tile';
 import Tiles from './Tiles';
 
-const Recommended = ({ recommended }: { recommended: RecommendedSongs[] }) => {
+interface RecommendedProps extends RecommendedSongs {
+  senderProfile?: Profile;
+}
+
+const Recommended = ({ recommended }: { recommended: RecommendedProps[] }) => {
   const scrollButtons = recommended.length > 5;
   const show = recommended.length > 0;
+  const timeFormatter = new Intl.RelativeTimeFormat();
 
   return (
     <>
@@ -13,10 +18,36 @@ const Recommended = ({ recommended }: { recommended: RecommendedSongs[] }) => {
         <Stack spacing={3}>
           <Tiles title="Recommended" scrollButtons={scrollButtons}>
             {recommended.map((recommended) => {
+              const date = new Date();
+              const currentTimestamp = date.getTime();
+              const createdAt = new Date(recommended.createdAt);
+              const createdAtTimestamp = createdAt.getTime();
+              const diffTimestamp = currentTimestamp - createdAtTimestamp;
+
+              const diffSeconds = Math.floor(diffTimestamp / 1000);
+              const diffMinutes = Math.floor(diffSeconds / 60);
+              const diffHours = Math.floor(diffMinutes / 60);
+              const diffDays = Math.floor(diffHours / 24);
+              const diffWeeks = Math.floor(diffDays / 7);
+              const diffMonths = Math.floor(diffWeeks / 4);
+
+              let timePassed = '';
+              if (diffSeconds < 60) {
+                timePassed = timeFormatter.format(-diffSeconds, 'seconds');
+              } else if (diffMinutes < 60) {
+                timePassed = timeFormatter.format(-diffMinutes, 'minutes');
+              } else if (diffHours < 24) {
+                timePassed = timeFormatter.format(-diffHours, 'hours');
+              } else if (diffDays < 7) {
+                timePassed = timeFormatter.format(-diffDays, 'days');
+              } else if (diffWeeks < 4) {
+                timePassed = timeFormatter.format(-diffWeeks, 'weeks');
+              } else {
+                timePassed = timeFormatter.format(-diffMonths, 'months');
+              }
               return (
-                <>
+                <Stack key={recommended.id}>
                   <Tile
-                    key={recommended.id}
                     uri={recommended.uri}
                     trackId={recommended.trackId}
                     image={recommended.image}
@@ -27,8 +58,10 @@ const Recommended = ({ recommended }: { recommended: RecommendedSongs[] }) => {
                     artistUri={recommended.albumUri}
                     explicit={recommended.explicit}
                     recommend={show}
+                    // recommendedBy={recommended.senderProfile}
                   />
-                </>
+                  <Text fontSize={['xs', 's']}>{timePassed}</Text>
+                </Stack>
               );
             })}
           </Tiles>
