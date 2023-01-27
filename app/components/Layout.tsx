db63c0ae35e53d18abd708a46d5f34bfce20b8a4
Daniel <daniel.valdecantos@gmail.com>
2023-01-26 17:31:09 -0800

    feat: add purple to danica's profile

--- app/components/Layout.tsx
+++ app/components/Layout.tsx
@@ -10,17 +10,22 @@ type LayoutProps = {
 
 const Layout = ({ authorized, children }: PropsWithChildren<LayoutProps>) => {
   const [isNya, setIsNya] = useState(false);
+  const [isDanica, setIsDanica] = useState(false);
   const { pathname } = useLocation();
   useEffect(() => {
     pathname === '/02mm0eoxnifin8xdnqwimls4y' ? setIsNya(true) : setIsNya(false);
   }, [pathname]);
+  useEffect(() => {
+    pathname === '/danicadboo' ? setIsDanica(true) : setIsDanica(false);
+  }, [pathname]);
+  const bgGradient = isNya
+    ? 'linear(to-t, #050404 40%, pink 110%)'
+    : isDanica
+    ? 'linear(to-t, #050404 40%, #563776 110%)'
+    : 'none';
 
   return (
-    <Flex
-      justify="center"
-      bgGradient={isNya ? 'linear(to-t, #050404 40%, pink 110%)' : 'none'}
-      bgAttachment="fixed"
-    >
+    <Flex justify="center" bgGradient={bgGradient} bgAttachment="fixed">
       <Box w={{ base: '100vw', sm: '450px', md: '750px', xl: '1100px' }} px={13}>
         <Nav authorized={authorized} />
         {children}
