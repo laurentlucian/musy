1d7365a4d91712471977d1ef888746a4bdd6524d
Daniel <daniel.valdecantos@gmail.com>
2023-01-29 12:39:02 -0800

    fix: remove tooltip type error
    explicitly destructure the props we use. I want to clear the error :)

--- app/components/Tooltip.tsx
+++ app/components/Tooltip.tsx
@@ -2,15 +2,26 @@ import type { TooltipProps } from '@chakra-ui/react';
 import { Tooltip as ChakraTooltip } from '@chakra-ui/react';
 import { forwardRef, useColorModeValue } from '@chakra-ui/system';
 
-const Tooltip = forwardRef<TooltipProps, 'div'>(({ children, ...props }, ref) => {
-  const bg = useColorModeValue('music.800', 'music.100');
-  const color = useColorModeValue('music.100', 'music.800');
+const Tooltip = forwardRef<TooltipProps, 'div'>(
+  ({ children, openDelay, label, placement, ...props }, ref) => {
+    const bg = useColorModeValue('music.800', 'music.100');
+    const color = useColorModeValue('music.100', 'music.800');
 
-  return (
-    <ChakraTooltip ref={ref} bg={bg} color={color} fontSize="12px" {...props}>
-      {children}
-    </ChakraTooltip>
-  );
-});
+    return (
+      <ChakraTooltip
+        ref={ref}
+        bg={bg}
+        color={color}
+        fontSize="12px"
+        openDelay={openDelay}
+        label={label}
+        placement={placement}
+        // {...props}
+      >
+        {children}
+      </ChakraTooltip>
+    );
+  },
+);
 
 export default Tooltip;
