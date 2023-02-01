591a479c3c92adf7c2fefa924ed5fce4ae437f83
Daniel <daniel.valdecantos@gmail.com>
2023-01-31 23:58:37 -0800

    feat: add delete button to recommended tiles

--- app/components/tiles/Recommended.tsx
+++ app/components/tiles/Recommended.tsx
@@ -1,4 +1,4 @@
-import { Image, Link, Stack, Text } from '@chakra-ui/react';
+import { IconButton, Image, Link, Stack, Text } from '@chakra-ui/react';
 
 import type { Profile, RecommendedSongs } from '@prisma/client';
 import { Minus } from 'iconsax-react';
@@ -6,10 +6,12 @@ import { Minus } from 'iconsax-react';
 import { timeSince } from '~/lib/utils';
 
 import Tile from '../Tile';
-import RecommendActions from './RecommendActions';
 import Tiles from './Tiles';
 
-// import RecommendActions from './RecommendActions';
+import type { action } from '~/routes/$id/removeRecommend'; // why is this value never read?
+import { useTypedFetcher } from 'remix-typedjson';
+import { useParams } from '@remix-run/react';
+
 type RecommendedProps = RecommendedSongs & {
   sender: Profile;
 };
@@ -17,6 +19,9 @@ type RecommendedProps = RecommendedSongs & {
 const Recommended = ({ recommended }: { recommended: RecommendedProps[] }) => {
   const scrollButtons = recommended.length > 5;
   const show = true;
+  const { id } = useParams();
+  const fetcher = useTypedFetcher<typeof action>();
+  const action = `/${id}/removeRecommend`;
 
   return (
     <>
@@ -24,6 +29,12 @@ const Recommended = ({ recommended }: { recommended: RecommendedProps[] }) => {
         <Stack spacing={3}>
           <Tiles title="Recommended" scrollButtons={scrollButtons}>
             {recommended.map((recommended) => {
+              const removeFromRecommended = () => {
+                fetcher.submit(
+                  { trackId: recommended.trackId },
+                  { action, method: 'post', replace: true },
+                );
+              };
               return (
                 <Stack key={recommended.id}>
                   <Tile
@@ -39,28 +50,33 @@ const Recommended = ({ recommended }: { recommended: RecommendedProps[] }) => {
                     preview_url={recommended.preview_url} // old recommended tracks dont have preview url :(((
                     link={recommended.link}
                   />
-
-                  <Stack direction="row">
-                    <RecommendActions
-                      recommendedBy={recommended.sender}
-                      trackId={recommended.trackId}
-                    />
-                    <Link href={`/${recommended.senderId}`}>
-                      <Image
-                        borderRadius="full"
-                        src={recommended.sender.image}
-                        boxSize="40px"
-                        mr="5px"
-                      />
-                    </Link>
-                    <Stack>
-                      <Link href={`/${recommended.senderId}`} _hover={{ textDecor: 'none' }}>
-                        <Text fontSize={['10px', '11px']}>{recommended.sender.name}</Text>
+                  <Stack direction="row" justify="space-between">
+                    <Stack direction="row">
+                      <Link href={`/${recommended.senderId}`}>
+                        <Image
+                          borderRadius="full"
+                          src={recommended.sender.image}
+                          boxSize="40px"
+                          mr="5px"
+                        />
                       </Link>
-                      <Text fontSize={['9px', '10px']} opacity={0.6}>
-                        {timeSince(recommended.createdAt)}
-                      </Text>
+                      <Stack>
+                        <Link href={`/${recommended.senderId}`} _hover={{ textDecor: 'none' }}>
+                          <Text fontSize={['10px', '11px']}>{recommended.sender.name}</Text>
+                        </Link>
+                        <Text fontSize={['9px', '10px']} opacity={0.6}>
+                          {timeSince(recommended.createdAt)}
+                        </Text>
+                      </Stack>
                     </Stack>
+                    <IconButton
+                      p={0}
+                      variant="ghost"
+                      aria-label="remove from recommended"
+                      _hover={{ color: 'red' }}
+                      icon={<Minus size="32" />}
+                      onClick={removeFromRecommended}
+                    />
                   </Stack>
                 </Stack>
               );
