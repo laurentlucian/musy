83645d316e63ba03078a39a62d4fd1df4c578625
ImLeugim <anarchxzx@gmail.com>
2023-02-01 21:04:09 -0800

    style: add icon to appearance settings

--- app/routes/settings/appearance.tsx
+++ app/routes/settings/appearance.tsx
@@ -14,6 +14,7 @@ import {
 
 import TimeRangePicker from '~/components/settings/TimeRangePicker';
 import { RadioButtons } from '~/lib/theme/components/SettingsRadio';
+import { Moon, Sun1 } from 'iconsax-react';
 
 // changes color mode but when navigating to new page it changes color back unless you refresh before route change
 
@@ -24,10 +25,23 @@ const Appearance = () => {
   const color = useColorModeValue('music.800', 'white');
   const defaultValue = color === 'music.800' ? 'light' : 'dark';
   const options = [
-    { name: 'dark', value: 'dark' },
-    { name: 'light', value: 'light' },
+    { name: 'Dark', value: 'dark' },
+    { name: 'Light', value: 'light' },
     // { name: 'system', value: 'system' }, //<---- will return when we can store a default value
   ];
+  console.log('selection', defaultValue);
+  const icon =
+    defaultValue === 'dark' ? (
+      <Moon size="24" color="#1DB954" variant="Bold" />
+    ) : (
+      <Sun1 size="24" color="#1DB954" variant="Bold" />
+    );
+  const icon2 =
+    defaultValue === 'dark' ? (
+      <Sun1 size="24" color="#555555" variant="Bold" />
+    ) : (
+      <Moon size="24" color="#555555" variant="Bold" />
+    );
 
   const { getRadioProps, getRootProps } = useRadioGroup({
     defaultValue, // fix default value to be stored value
@@ -45,7 +59,7 @@ const Appearance = () => {
     <Stack spacing={5} w={['unset', '400px']}>
       <Stack direction="row" alignItems="center">
         <Text fontSize={['sm', 'md']} color={color}>
-          scheduled:
+          Scheduled:
         </Text>
         <Button size={['xs', 'sm']} aria-label="off" onClick={() => setScheduled(false)}>
           off
@@ -64,11 +78,17 @@ const Appearance = () => {
               <HStack justifyContent="space-between">
                 {radio.isChecked ? (
                   <>
-                    {name}
+                    <HStack gap={2} w="100%">
+                      {icon}
+                      <>{name}</>
+                    </HStack>
                     <Check />
                   </>
                 ) : (
-                  <>{name}</>
+                  <HStack gap={2} w="100%">
+                    {icon2}
+                    <>{name}</>
+                  </HStack>
                 )}
               </HStack>
             </RadioButtons>
