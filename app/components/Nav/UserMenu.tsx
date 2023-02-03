69726d5229f0074d47d47ad9c3bae980771268b3
Daniel <daniel.valdecantos@gmail.com>
2023-02-03 01:30:21 -0800

    fix: render menu in portal

--- app/components/Nav/UserMenu.tsx
+++ app/components/Nav/UserMenu.tsx
@@ -21,6 +21,7 @@ import {
   useDisclosure,
   Divider,
   Stack,
+  Portal,
 } from '@chakra-ui/react';
 import { Form, useNavigate } from '@remix-run/react';
 import { Moon, Profile2User, Setting2, Sun1 } from 'iconsax-react';
@@ -186,52 +187,54 @@ const UserMenu = ({ isSmallScreen }: UserActionsConfig) => {
       ) : (
         <Menu placement="bottom-end">
           <MenuButton aria-label="your actions" as={iconButton} variant="unstyled" isRound />
-          <MenuList overflowX="clip" bg={bg} boxShadow="0px 0px 10px 2px rgba(117,117,117,0.69)">
-            <MenuItem
-              as="a"
-              href={`/${currentUser?.userId}`}
-              icon={icon}
-              ml="-5px"
-              mr="8px"
-              bg={bg}
-              color={color}
-            >
-              {currentUser?.name}
-            </MenuItem>
-            <MenuItem
-              icon={<Profile2User variant="Bold" />}
-              onClick={onClickFriends}
-              bg={bg}
-              color={color}
-            >
-              friends
-            </MenuItem>
-            <MenuDivider border={`solid ${color} 1px`} w="90%" ml="9px" opacity={0.2} />
-            <MenuItem icon={<Setting2 />} onClick={onClickSettings} bg={bg} color={color}>
-              settings
-            </MenuItem>
-            <MenuItem
-              onClick={toggleColorMode}
-              icon={colorMode === 'light' ? <Moon variant="Bold" /> : <Sun1 variant="Bold" />}
-              closeOnSelect={false}
-              bg={bg}
-              color={color}
-            >
-              toggle theme
-            </MenuItem>
-            <MenuDivider border={`solid ${color} 1px`} w="90%" ml="9px" opacity={0.2} />
-            <Form action="/logout" method="post">
+          <Portal>
+            <MenuList overflowX="clip" bg={bg} boxShadow="0px 0px 10px 2px rgba(117,117,117,0.69)">
               <MenuItem
-                icon={<LogOut transform="scale(-1)" />}
-                _hover={{ bgColor: 'red.500', color: 'white' }}
-                type="submit"
+                as="a"
+                href={`/${currentUser?.userId}`}
+                icon={icon}
+                ml="-5px"
+                mr="8px"
                 bg={bg}
                 color={color}
               >
-                log out
+                {currentUser?.name}
               </MenuItem>
-            </Form>
-          </MenuList>
+              <MenuItem
+                icon={<Profile2User variant="Bold" />}
+                onClick={onClickFriends}
+                bg={bg}
+                color={color}
+              >
+                friends
+              </MenuItem>
+              <MenuDivider border={`solid ${color} 1px`} w="90%" ml="9px" opacity={0.2} />
+              <MenuItem icon={<Setting2 />} onClick={onClickSettings} bg={bg} color={color}>
+                settings
+              </MenuItem>
+              <MenuItem
+                onClick={toggleColorMode}
+                icon={colorMode === 'light' ? <Moon variant="Bold" /> : <Sun1 variant="Bold" />}
+                closeOnSelect={false}
+                bg={bg}
+                color={color}
+              >
+                toggle theme
+              </MenuItem>
+              <MenuDivider border={`solid ${color} 1px`} w="90%" ml="9px" opacity={0.2} />
+              <Form action="/logout" method="post">
+                <MenuItem
+                  icon={<LogOut transform="scale(-1)" />}
+                  _hover={{ bgColor: 'red.500', color: 'white' }}
+                  type="submit"
+                  bg={bg}
+                  color={color}
+                >
+                  log out
+                </MenuItem>
+              </Form>
+            </MenuList>
+          </Portal>
         </Menu>
       )}
     </>
