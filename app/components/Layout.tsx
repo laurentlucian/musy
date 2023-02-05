8c7cb127d90690dcbec1d4bacb22a623dd5cc9c6
Daniel <daniel.valdecantos@gmail.com>
2023-02-04 17:34:12 -0800

    fix: change to uppercase folder name

--- app/components/Layout.tsx
+++ app/components/Layout.tsx
@@ -4,7 +4,7 @@ import { type PropsWithChildren, useMemo } from 'react';
 import { Box, Flex, useColorModeValue } from '@chakra-ui/react';
 
 import MobileSearchButton from './menu/MobileSearchButton';
-import Nav from './nav/Nav';
+import Nav from './Nav/Nav';
 
 type LayoutProps = {
   authorized: boolean;
