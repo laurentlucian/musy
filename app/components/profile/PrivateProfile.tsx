350e1f1b7ba6abff236c48a7eb0ac8f6671458d4
ImLeugim <anarchxzx@gmail.com>
2023-02-06 20:53:01 -0800

    style: icon size bigger

--- app/components/profile/PrivateProfile.tsx
+++ app/components/profile/PrivateProfile.tsx
@@ -9,7 +9,7 @@ const PrivateProfile = ({ name }: { name: string }) => {
 
   const ghost = (
     <motion.div animate={{ opacity: [0, 1, 0, 1] }} transition={{ duration: 5, loop: Infinity }}>
-      <Ghost size="64" color={green} />
+      <Ghost size="210" color={green} />
     </motion.div>
   );
   console.log(name, 'user');
@@ -17,11 +17,8 @@ const PrivateProfile = ({ name }: { name: string }) => {
     <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 3 }}>
       <Stack align="center">
         {ghost}
-        <Text textAlign="center" fontSize="64px" fontWeight="bold">
-          oops
-        </Text>
         <Text textAlign="center" opacity=".5">
-          the content for {name} are hidden
+          {name}'s profile is private
         </Text>
         <Button
           w="300px"
