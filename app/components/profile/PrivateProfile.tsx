cfe1d124eabcb1b11b64d034f9e303eb78a9c4bd
ImLeugim <anarchxzx@gmail.com>
2023-02-06 16:37:56 -0800

    style : remove ghost icon

--- app/components/profile/PrivateProfile.tsx
+++ app/components/profile/PrivateProfile.tsx
@@ -6,24 +6,22 @@ import { Ghost, LockCircle } from 'iconsax-react';
 const PrivateProfile = () => {
   const green = '#1DB954';
 
-  const ghost = (
-    <motion.div animate={{ opacity: [0, 1, 0, 1] }} transition={{ duration: 5, loop: Infinity }}>
-      <Ghost size="32" color={green} />
-    </motion.div>
-  );
+  //   ghost icon animation if needed
+  //   const ghost = (
+  //     <motion.div animate={{ opacity: [0, 1, 0, 1] }} transition={{ duration: 5, loop: Infinity }}>
+  //       <Ghost size="32" color={green} />
+  //     </motion.div>
+  //   );
 
   return (
     <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 3 }}>
       <Stack w="100%" align="center">
         <LockCircle size="32" color={green} variant="Bulk" />
         <HStack>
-          {ghost}
           <Text>profile is hidden</Text>
-          {ghost}
         </HStack>
-
         <Button _hover={{ color: 'spotify.green' }} onClick={() => window.history.back()}>
-        go back
+          go back
         </Button>
       </Stack>
     </motion.div>
