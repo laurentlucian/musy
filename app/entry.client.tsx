78734c7fbdca0b0269210904c1fb967f6933204c
Pierre Ortega <pierreandreisortega@gmail.com>
2023-02-12 16:03:04 -0800

    fix: emotion rehydration on root

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,33 +1,33 @@
 import { RemixBrowser } from '@remix-run/react';
 import * as React from 'react';
-import { hydrateRoot } from 'react-dom/client';
+import { hydrate } from 'react-dom';
 
 import { CacheProvider } from '@emotion/react';
 
-import { ClientStyleContext } from './lib/emotion/context';
-import { createEmotionCache, defaultCache } from './lib/emotion/createEmotionCache';
+import { ClientStyleContext } from '~/lib/emotion/context';
+import { createEmotionCache } from '~/lib/emotion/createEmotionCache';
 
 interface ClientCacheProviderProps {
   children: React.ReactNode;
 }
 
-const ClientCacheProvider = ({ children }: ClientCacheProviderProps) => {
-  const [cache, setCache] = React.useState(defaultCache);
+function ClientCacheProvider({ children }: ClientCacheProviderProps) {
+  const [cache, setCache] = React.useState(createEmotionCache());
 
-  const reset = () => {
+  function reset() {
     setCache(createEmotionCache());
-  };
+  }
 
   return (
     <ClientStyleContext.Provider value={{ reset }}>
       <CacheProvider value={cache}>{children}</CacheProvider>
     </ClientStyleContext.Provider>
   );
-};
+}
 
-hydrateRoot(
-  document,
+hydrate(
   <ClientCacheProvider>
     <RemixBrowser />
   </ClientCacheProvider>,
+  document,
 );
