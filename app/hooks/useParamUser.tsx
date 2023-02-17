3ac61942c2a1fe306e91ed81f855804a1ac97727
Daniel <daniel.valdecantos@gmail.com>
2023-02-16 23:45:17 -0800

    feat: update hook to include more profile data
    add settings, liked, and theme

--- app/hooks/useParamUser.tsx
+++ app/hooks/useParamUser.tsx
@@ -1,8 +1,14 @@
 import { useMatches } from '@remix-run/react';
 
-import type { Profile } from '@prisma/client';
+import type { Profile, Settings, Theme, Track } from '@prisma/client';
 
-const useParamUser = (): Profile | null => {
+const useParamUser = ():
+  | (Profile & {
+      liked: { trackId: string }[];
+      settings: (Settings & { profileSong: Track }) | null;
+      theme: Theme | null;
+    })
+  | null => {
   const matches = useMatches();
   const route = matches.find((match) => match.id === 'routes/$id');
   if (!route || !route.data) return null;
