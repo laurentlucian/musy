b0618e1704ec547108ebe8e2325ba6ae1a073820
Daniel <daniel.valdecantos@gmail.com>
2023-02-18 11:43:27 -0800

    fix: add null check to recent tracks

--- app/routes/$id.tsx
+++ app/routes/$id.tsx
@@ -116,7 +116,7 @@ export const loader = async ({ params, request }: LoaderArgs) => {
   const recentDb = await prisma.recentSongs.findMany({
     orderBy: { playedAt: 'desc' },
     select: { playedAt: true, track: { select: { duration: true } } },
-    where: { userId: id },
+    where: { AND: [{ track: { uri: { startsWith: 'spotify' } } }, { userId: id }] }, // added a where track exists as a null check :),
   });
 
   const { searchParams } = new URL(request.url);
