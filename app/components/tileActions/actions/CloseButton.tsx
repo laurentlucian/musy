d806f39769ce13211020af5bbb670716f67f692f
Daniel <daniel.valdecantos@gmail.com>
2023-02-26 10:20:33 -0800

    fix(expanded tiles): adjust close button position

--- app/components/tileActions/actions/CloseButton.tsx
+++ app/components/tileActions/actions/CloseButton.tsx
@@ -10,7 +10,15 @@ const CloseButton = () => {
     if (e.code === 'Escape') onClose();
   });
   return (
-    <Button variant="ghost" pos="fixed" bottom="10%" right="50%" left="50%" onClick={onClose}>
+    <Button
+      variant="ghost"
+      pos="fixed"
+      bottom={['5%', '10%']}
+      right={['unset', '50%']}
+      left={['unset', '50%']}
+      alignSelf="center"
+      onClick={onClose}
+    >
       close
     </Button>
   );
