e1729530eea2cb745a6c0e2951b3c65abe4f00b4
Daniel <daniel.valdecantos@gmail.com>
2023-02-25 16:10:57 -0800

    fix: render User menu higher

--- app/components/nav/UserMenu.tsx
+++ app/components/nav/UserMenu.tsx
@@ -270,6 +270,7 @@ const UserMenu = ({ isSmallScreen, pathname }: UserActionsConfig) => {
               bg={bg}
               boxShadow="0px 0px 10px 2px rgba(117,117,117,0.69)"
               rounded="xl"
+              zIndex={99} 
             >
               <MenuItem
                 as="a"
