a177d35f8924d2ef2b7ba26ec2e796033182e2ff
Daniel <daniel.valdecantos@gmail.com>
2023-03-16 00:09:43 -0700

    style: add more styles to tabs

--- app/components/FriendsTabs.tsx
+++ app/components/FriendsTabs.tsx
@@ -17,7 +17,9 @@ import type { loader } from '~/routes/friends';
 
 const FriendsTabs = ({ children }: { children: ReactNode }) => {
   const { currentFriends, pendingFriends } = useTypedLoaderData<typeof loader>();
-  const friendsTabImg = currentFriends ? <Image boxSize="15px" src="/users.svg" /> : null;
+  const friendsTabImg = currentFriends ? (
+    <Image boxSize="20px" src="/users.svg" mr="20px" alignSelf="center" />
+  ) : null;
   const color = useColorModeValue('#161616', '#EEE6E2');
   const bg = useColorModeValue('#EEE6E2', '#050404');
 
@@ -25,11 +27,12 @@ const FriendsTabs = ({ children }: { children: ReactNode }) => {
     <Stack pt={{ base: '60px', xl: 0 }} pb="100px" spacing={3} w="100%" h="100%" px={['4px', 0]}>
       <Tabs align="start" colorScheme="music" variant="soft-rounded">
         <TabList mb="5px">
-          <Tab color={color} bg={bg}>
-            {friendsTabImg} friends ~ {currentFriends.length}
+          {friendsTabImg}
+          <Tab color={color} bg={bg} mr="20px">
+            friends {currentFriends.length ? currentFriends.length : ''}
           </Tab>
           <Tab color={color} bg={bg}>
-            requests ~ {pendingFriends.length}
+            requests {pendingFriends.length ? pendingFriends.length : ''}
           </Tab>
         </TabList>
         <Divider bgColor="spotify.green" />
