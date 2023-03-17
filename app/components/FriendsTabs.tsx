8b1ab5774290003d6b0bfd04fe389cac83c0358c
Daniel <daniel.valdecantos@gmail.com>
2023-03-16 23:31:38 -0700

    style: clean up component

--- app/components/FriendsTabs.tsx
+++ app/components/FriendsTabs.tsx
@@ -17,9 +17,6 @@ import type { loader } from '~/routes/friends';
 
 const FriendsTabs = ({ children }: { children: ReactNode }) => {
   const { currentFriends, pendingFriends } = useTypedLoaderData<typeof loader>();
-  const friendsTabImg = currentFriends ? (
-    <Image boxSize="20px" src="/users.svg" mr="20px" alignSelf="center" />
-  ) : null;
   const color = useColorModeValue('#161616', '#EEE6E2');
   const bg = useColorModeValue('#EEE6E2', '#050404');
 
@@ -27,7 +24,7 @@ const FriendsTabs = ({ children }: { children: ReactNode }) => {
     <Stack pt={{ base: '60px', xl: 0 }} pb="100px" spacing={3} w="100%" h="100%" px={['4px', 0]}>
       <Tabs align="start" colorScheme="music" variant="soft-rounded">
         <TabList mb="5px">
-          {friendsTabImg}
+          <Image boxSize="20px" src="/users.svg" mr="20px" alignSelf="center" />
           <Tab color={color} bg={bg} mr="20px">
             friends {currentFriends.length ? currentFriends.length : ''}
           </Tab>
