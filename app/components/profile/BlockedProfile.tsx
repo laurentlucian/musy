805b7643868c7605188c2e4c235ffa3decb00b89
ImLeugim <anarchxzx@gmail.com>
2023-03-22 23:35:23 -0700

    style: block icon changed

--- app/components/profile/BlockedProfile.tsx
+++ app/components/profile/BlockedProfile.tsx
@@ -1,14 +1,12 @@
-import { Lock } from 'react-feather';
-
 import { Button, HStack, Stack, Text } from '@chakra-ui/react';
 
 import { motion } from 'framer-motion';
+import { Forbidden } from 'iconsax-react';
 
 const BlockedProfile = ({ amIBlocked, name }: { amIBlocked: boolean; name: string }) => {
-  const green = '#1DB954';
   const block = (
     <motion.div animate={{ opacity: [0, 1, 0, 1] }} transition={{ duration: 5, loop: Infinity }}>
-      <Lock size="30" color={green} />
+      <Forbidden size="30" color="red" />
     </motion.div>
   );
   return (
