30ac55b3423f45263e3bf76422f2a13975925786
ImLeugim <anarchxzx@gmail.com>
2023-03-24 22:36:59 -0700

    style: freinds tab animation

--- app/components/FriendsTabs.tsx
+++ app/components/FriendsTabs.tsx
@@ -1,6 +1,8 @@
+import { useEffect } from 'react';
 import type { ReactNode } from 'react';
 
 import {
+  Badge,
   Divider,
   Image,
   Stack,
@@ -8,12 +10,53 @@ import {
   TabList,
   TabPanels,
   Tabs,
+  Text,
   useColorModeValue,
 } from '@chakra-ui/react';
 
+import { motion, useCycle } from 'framer-motion';
 import { useTypedLoaderData } from 'remix-typedjson';
 
 import type { loader } from '~/routes/friends';
+
+const NotificationBadge = ({ count }: { count: number }) => {
+  const [isFlashing, toggleFlash] = useCycle(false, true);
+
+  // Restart the animation on mount
+  useEffect(() => {
+    toggleFlash();
+  }, [toggleFlash]);
+
+  return (
+    <motion.div
+      transition={{ duration: 1 }}
+      style={{
+        position: 'absolute',
+        right: '-20px',
+        top: '-20px',
+        zIndex: 1,
+      }}
+      animate={isFlashing ? { scale: [1, 1.2, 1], opacity: [1, 0.7, 1] } : {}}
+      onAnimationComplete={() => toggleFlash()}
+    >
+      <Badge bg="red" borderRadius="full">
+        {count}
+      </Badge>
+    </motion.div>
+  );
+};
+
+const PendingFriendsNotification = ({ pendingFriends }: { pendingFriends?: number }) => {
+  return (
+    <div style={{ position: 'relative' }}>
+      <Text>
+        requests
+        {/* {pendingFriends.length ? pendingFriends.length : ''} */}
+      </Text>
+      {pendingFriends ? <NotificationBadge count={pendingFriends} /> : null}
+    </div>
+  );
+};
 
 const FriendsTabs = ({ children }: { children: ReactNode }) => {
   const { currentFriends, pendingFriends } = useTypedLoaderData<typeof loader>();
@@ -22,14 +65,14 @@ const FriendsTabs = ({ children }: { children: ReactNode }) => {
 
   return (
     <Stack pt={{ base: '60px', xl: 0 }} pb="100px" spacing={3} w="100%" h="100%" px={['4px', 0]}>
-      <Tabs align="start" colorScheme="music" variant="soft-rounded">
+      <Tabs align="start" colorScheme="green" variant="soft-rounded" size="sm">
         <TabList mb="5px">
-          <Image boxSize="20px" src="/users.svg" mr="20px" alignSelf="center" />
+          <Image boxSize="15px" src="/users.svg" mr="20px" alignSelf="center" />
           <Tab color={color} bg={bg} mr="20px">
             friends {currentFriends.length ? currentFriends.length : ''}
           </Tab>
           <Tab color={color} bg={bg}>
-            requests {pendingFriends.length ? pendingFriends.length : ''}
+            <PendingFriendsNotification pendingFriends={pendingFriends.length} />
           </Tab>
         </TabList>
         <Divider bgColor="spotify.green" />
