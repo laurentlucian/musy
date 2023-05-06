3816668f5c81093756fdd5f90d3c84f7a4a70932
danielbuva <daniel.valdecantos@gmail.com>
2023-05-05 22:59:04 -0700

    style: move rateRecommend endpoint and fix url

--- app/components/tiles/RecommendRatingForm.tsx
+++ app/components/tiles/RecommendRatingForm.tsx
@@ -1,40 +1,40 @@
-import { useParams } from '@remix-run/react';
-
 import { FormControl, FormLabel, useRadioGroup, HStack, useColorModeValue } from '@chakra-ui/react';
 
 import { useTypedFetcher } from 'remix-typedjson';
 
 import { RadioButtons } from '~/lib/theme/components/RatingRadio';
-import type { action } from '~/routes/$id/rateRecommend';
+import type { action } from '~/routes/api/rateRecommend';
+
+const options = [
+  { name: '1', value: '1' },
+  { name: '2', value: '2' },
+  { name: '3', value: '3' },
+  { name: '4', value: '4' },
+  { name: '5', value: '5' },
+  { name: '6', value: '6' },
+  { name: '7', value: '7' },
+  { name: '8', value: '8' },
+  { name: '9', value: '9' },
+  { name: '10', value: '10' },
+];
 
 const RecommendRatingForm = ({ rating, sender }: { rating?: string; sender: string }) => {
   const color = useColorModeValue('#161616', '#EEE6E2');
   const bg = useColorModeValue('music.200', 'music.700');
   const fetcher = useTypedFetcher<typeof action>();
-  const { id } = useParams();
 
-  const onChange = (value: string) => {
-    const action = `/${id}/removeRecommend`;
-    fetcher.submit({ rating: value }, { action, method: 'post', replace: true });
+  const onChange = (rating: string) => {
+    fetcher.submit({ rating }, { action: '/api/rateRecommend', method: 'post', replace: true });
   };
-  const options = [
-    { name: '1', value: '1' },
-    { name: '2', value: '2' },
-    { name: '3', value: '3' },
-    { name: '4', value: '4' },
-    { name: '5', value: '5' },
-    { name: '6', value: '6' },
-    { name: '7', value: '7' },
-    { name: '8', value: '8' },
-    { name: '9', value: '9' },
-    { name: '10', value: '10' },
-  ];
+
   const { getRadioProps, getRootProps } = useRadioGroup({
     defaultValue: rating ?? '5',
     name: 'rating',
     onChange: (value) => onChange(value),
   });
+
   const group = getRootProps();
+
   return (
     <FormControl
       display="flex"
