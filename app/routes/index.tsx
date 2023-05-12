b00a01e3e524a4e302374f4efa1700501c720625
dani <daniel.valdecantos@gmail.com>
2023-05-11 18:08:38 -0700

    feat: start landing page

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,6 +1,27 @@
+import { Button, Heading, Stack, Text } from '@chakra-ui/react';
+
 import { redirect } from 'remix-typedjson';
 
-const Index = () => null; // @todo landing page
+const Index = () => {
+  return (
+    <Stack>
+      <Stack h="100vh" alignItems="center" justifyContent="center">
+        <Heading>musy</Heading>
+        <Text>music shared easy</Text>
+      </Stack>
+      <Stack h="100vh">
+        <Heading>Send songs to your friends</Heading>
+        <Text>recommend or add to their queue</Text>
+      </Stack>
+      <Stack h="100vh" alignItems="center">
+        <Heading>See what your friends are listening to</Heading>
+      </Stack>
+      <Stack h="100vh">
+        <Button>Log in with Spotify</Button>
+      </Stack>
+    </Stack>
+  );
+};
 
 export const loader = () => redirect('/home');
 
