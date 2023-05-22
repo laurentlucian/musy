62577887decc7be4bf316ba4ccb0c837673865ad
Laurent <laurent.lucian@gmail.com>
2023-05-22 10:14:41 -0700

    chore: add unstable_dev to use hmr

--- remix.config.js
+++ remix.config.js
@@ -3,17 +3,12 @@
  */
 
 module.exports = {
-  ignoredRouteFiles: ["**/.*"],
+  future: {
+    unstable_dev: true,
+  },
+  ignoredRouteFiles: ['**/.*'],
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // serverBuildPath: "build/index.js",
   // publicPath: "/build/",
 };
-// module.exports = {
-//   appDirectory: "app",
-//   browserBuildDirectory: "public/build",
-//   publicPath: "/build/",
-//   serverBuildDirectory: "build",
-//   devServerPort: 8002,
-//   ignoredRouteFiles: [".*"]
-// };
