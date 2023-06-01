6f41f7ae7a9cc30927db5bb7eef182d516b8f347
Laurent <laurent.lucian@gmail.com>
2023-05-31 19:54:16 -0700

    feat: auto follow
    @todo replace with onboarding user suggestions

--- app/services/scheduler/jobs/user.server.ts
+++ app/services/scheduler/jobs/user.server.ts
@@ -201,6 +201,39 @@ export const userQ = Queue<{ userId: string }>(
       }
     }
 
+    const users = await prisma.user
+      .findMany({
+        select: {
+          id: true,
+        },
+        where: {
+          revoked: false,
+        },
+      })
+      .then((users) => users.map((u) => u.id));
+
+    const { body: isFollowing } = await spotify.isFollowingUsers(users);
+
+    const following = users.filter((_, i) => isFollowing[i]);
+
+    console.log('userQ -> adding following to db', userId, following.length);
+    for (const followingId of following) {
+      await prisma.follow.upsert({
+        create: {
+          // create if it doesn't exist -- if user unfollow in musy but keep in spotify, it'll auto follow again on next job run
+          followerId: userId,
+          followingId,
+        },
+        update: {}, // if it exists, do nothing
+        where: {
+          followingId_followerId: {
+            followerId: userId,
+            followingId,
+          },
+        },
+      });
+    }
+
     console.log('userQ -> completed', userId);
   },
   {
