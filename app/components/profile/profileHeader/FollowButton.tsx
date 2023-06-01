1f2fc434a63cdbbc590dee641e115662308193bc
danielbuva <daniel.valdecantos@gmail.com>
2023-06-01 12:43:43 -0500

    chore: remove unused logic

--- app/components/profile/profileHeader/FollowButton.tsx
+++ app/components/profile/profileHeader/FollowButton.tsx
@@ -1,12 +1,10 @@
 import { useParams } from '@remix-run/react';
 import { useState } from 'react';
 
-import { Button, IconButton, useColorModeValue } from '@chakra-ui/react';
+import { Button } from '@chakra-ui/react';
 
-import { UserAdd, UserMinus } from 'iconsax-react';
 import { useTypedFetcher } from 'remix-typedjson';
 
-import Tooltip from '~/components/Tooltip';
 import useSessionUser from '~/hooks/useSessionUser';
 import type { action as followAction } from '~/routes/api/user/follow';
 
@@ -16,13 +14,8 @@ const FollowButton = (props: { id?: string }) => {
   const userId = (props.id || params.id) as string;
   const isFollowingDefault = currentUser?.following.find((user) => userId === user.followingId);
   const [isFollowing, setIsfollowing] = useState<boolean>(!!isFollowingDefault);
-  const color = useColorModeValue('#161616', '#EEE6E2');
 
   const fetcher = useTypedFetcher<typeof followAction>();
-
-  const label = isFollowing ? 'Unfollow' : 'Follow';
-
-  const icon = isFollowing ? <UserMinus /> : <UserAdd />;
 
   return (
     <Button
@@ -44,29 +37,6 @@ const FollowButton = (props: { id?: string }) => {
     >
       {isFollowing ? 'Following' : 'Follow'}
     </Button>
-  );
-
-  return (
-    <Tooltip label={label}>
-      <IconButton
-        aria-label="follow"
-        variant="ghost"
-        isLoading={fetcher.formAction?.includes(userId)}
-        icon={icon}
-        color={isFollowing ? 'spotify.green' : color}
-        _hover={{ color: 'spotify.green' }}
-        onClick={(e) => {
-          e.preventDefault();
-          if (currentUser) {
-            setIsfollowing(!isFollowing);
-            fetcher.submit(
-              { currentUserId: currentUser.userId, isFollowing: String(isFollowing), userId },
-              { action: `/api/user/follow`, method: 'post', replace: true },
-            );
-          }
-        }}
-      />
-    </Tooltip>
   );
 };
 
