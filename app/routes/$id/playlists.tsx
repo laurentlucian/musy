5788bbe9c67b0e15ef1d46934abfe759e8d5c8bd
Laurent <laurent.lucian@gmail.com>
2023-07-04 14:44:07 -0700

    feat: add user/playlists

---
+++ app/routes/$id/playlists.tsx
@@ -0,0 +1,54 @@
+import type { LoaderArgs } from '@remix-run/server-runtime';
+
+import { Stack } from '@chakra-ui/react';
+
+import { typedjson, useTypedLoaderData } from 'remix-typedjson';
+import invariant from 'tiny-invariant';
+
+import TilesTrack from '~/components/tiles/TilesTrack';
+import { prisma } from '~/services/db.server';
+
+const ProfilePlaylists = () => {
+  const { playlists } = useTypedLoaderData<typeof loader>();
+
+  return (
+    <Stack spacing={8} pos="relative">
+      {playlists.map((playlist) => {
+        return (
+          <TilesTrack
+            key={playlist.id}
+            tracks={playlist.tracks.map((t) => t.track)}
+            title={playlist.name}
+          />
+        );
+      })}
+    </Stack>
+  );
+};
+
+export const loader = async ({ params }: LoaderArgs) => {
+  const id = params.id;
+  invariant(id, 'Missing params Id');
+
+  const playlists = await prisma.playlist.findMany({
+    include: {
+      tracks: {
+        include: {
+          track: true,
+        },
+        orderBy: {
+          addedAt: 'desc',
+        },
+        take: 20,
+      },
+    },
+
+    where: {
+      userId: id,
+    },
+  });
+
+  return typedjson({ playlists });
+};
+
+export default ProfilePlaylists;
