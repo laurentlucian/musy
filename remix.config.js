83e6a7226b92e9bc5db59b64742d5017fb2708cc
Laurent <laurent.lucian@gmail.com>
2023-10-21 03:39:15 -0700

    chore: remove v2 flags

--- remix.config.js
+++ remix.config.js
@@ -2,14 +2,6 @@ const { createRoutesFromFolders } = require('@remix-run/v1-route-convention');
 
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
-  future: {
-    v2_dev: true,
-    v2_errorBoundary: true,
-    v2_headers: true,
-    v2_meta: true,
-    v2_normalizeFormMethod: true,
-    v2_routeConvention: true,
-  },
   ignoredRouteFiles: ['**/.*'],
   routes(defineRoutes) {
     return createRoutesFromFolders(defineRoutes);
