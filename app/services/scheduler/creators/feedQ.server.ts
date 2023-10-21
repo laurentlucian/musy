df71d1375d0884cf9767e48b912ed72f8a72b2a4
Laurent <laurent.lucian@gmail.com>
2023-10-21 04:46:46 -0700

    chore: re add feed queue

--- app/services/scheduler/creators/feedQ.server.ts
+++ app/services/scheduler/creators/feedQ.server.ts
@@ -8,29 +8,26 @@ import { timestampToDate } from './userQ.server';
 const debugFeedQCreator = debug('feedQCreator');
 
 export const createFeedQ = async () => {
-  console.log('creating feedQ...');
   debugFeedQCreator('creating feedQ...');
 
   const getFeedQ = async () =>
     (await feedQ.getRepeatableJobs()).map((j) => [timestampToDate(j.next), j.key]);
 
-  // await feedQ.pause().catch(debugFeedQCreator); // pause all jobs before obliterating
+  await feedQ.pause().catch(debugFeedQCreator); // pause all jobs before obliterating
   await feedQ.obliterate({ force: true }).catch(() => debugFeedQCreator('obliterated userQ')); // https://github.com/taskforcesh/bullmq/issues/430
   const feedQs = await getFeedQ();
 
-  // if (feedQs.length === 0) {
-  //   // await feedQ.add('update_feed', null);
-  //   await feedQ.add('update_feed', null, {
-  //     backoff: {
-  //       delay: minutesToMs(1),
-  //       type: 'exponential',
-  //     },
-  //     removeOnComplete: true,
-  //     removeOnFail: true,
-  //     repeat: { every: minutesToMs(0.2) },
-  //   });
-  //   debugFeedQCreator('added feed job');
-  // }
-
-  debugFeedQCreator('feedQ completed');
+  if (feedQs.length === 0) {
+    // await feedQ.add('update_feed', null);
+    await feedQ.add('update_feed', null, {
+      backoff: {
+        delay: minutesToMs(1),
+        type: 'exponential',
+      },
+      removeOnComplete: true,
+      removeOnFail: true,
+      repeat: { every: minutesToMs(0.2) },
+    });
+    debugFeedQCreator('added 1 repeateable feed job');
+  }
 };
