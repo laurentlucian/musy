1e89695028c980b886b7001ba755d59662ffefbc
Laurent <laurent.lucian@gmail.com>
2023-11-18 10:56:33 -0800

    chore: upgrade remix & add dev tools

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,14 @@
 import { unstable_vitePlugin as remix } from '@remix-run/dev';
 
+// @ts-expect-error not typed yet
+import { remixDevTools } from 'remix-development-tools/vite';
 import { flatRoutes } from 'remix-flat-routes';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [
+    remixDevTools(),
     remix({
       ignoredRouteFiles: ['**/.*'],
       routes: async (defineRoutes) => {
