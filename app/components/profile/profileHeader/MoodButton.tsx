68c748df97d4304b31155bfec4e4b4496064d9fa
Laurent <laurent.lucian@gmail.com>
2024-01-21 12:57:00 -0800

    fix: use custom tooltip component

--- app/components/profile/profileHeader/MoodButton.tsx
+++ app/components/profile/profileHeader/MoodButton.tsx
@@ -1,12 +1,11 @@
 import { useParams } from '@remix-run/react';
 import { Fragment, useEffect } from 'react';
 
-import { TooltipContent, TooltipTrigger } from '@radix-ui/react-tooltip';
 import { useTypedFetcher } from 'remix-typedjson';
 
 import useIsMobile from '~/hooks/useIsMobile';
 import Waver from '~/lib/icons/Waver';
-import { Tooltip } from '~/lib/ui/tooltip';
+import { Tooltip, TooltipContent, TooltipTrigger } from '~/lib/ui/tooltip';
 import { timeSince } from '~/lib/utils';
 import type { action as moodAction } from '~/routes/api+/user+/profile+/mood';
 
@@ -34,18 +33,14 @@ const MoodButton = ({ mood, since }: { mood?: string | null; since?: Date }) => 
   return (
     <Tooltip disableHoverableContent={isSmallScreen}>
       <TooltipContent>{label}</TooltipContent>
-      <TooltipTrigger>
+      <TooltipTrigger aria-label='get mood' onClick={getMood}>
         <Fragment>
           {isLoading && <Waver />}
           {!isLoading && (
-            <button
-              className='flex h-fit cursor-pointer items-center justify-center p-0 text-[10px] md:text-[13px]'
-              aria-label='get mood'
-              onClick={getMood}
-            >
+            <p className='text-[10px] md:text-[13px]'>
               {text}
               <span className='pl-1 text-[9px] opacity-50 md:text-[10px]'>{timePassed}</span>
-            </button>
+            </p>
           )}
         </Fragment>
       </TooltipTrigger>
