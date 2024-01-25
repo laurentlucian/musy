8a0d37cabfe6872d0ecc5c5114e61b92b1c290ee
Laurent <laurent.lucian@gmail.com>
2024-01-25 11:07:49 -0800

    chore: add biome

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,8 +1,8 @@
 /** @type {import('@commitlint/types').UserConfig} */
 
 module.exports = {
-  extends: ['@commitlint/config-conventional'],
+  extends: ["@commitlint/config-conventional"],
   // add your own scope here if needed
   // "scope-enum": [2, "always", ["components", "pages", "utils"]],
-  'scope-case': [2, 'always', 'kebab-case'],
+  "scope-case": [2, "always", "kebab-case"],
 };
