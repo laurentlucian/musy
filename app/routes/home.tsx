b48724e94c8799b26f7e8e08259dafdb136a7c56
Laurent <laurent.lucian@gmail.com>
2024-12-01 16:46:15 -0800

    feat: top songs

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -9,28 +9,38 @@ export async function loader({ context }: Route.LoaderArgs) {
   return { leaderboard: await getTopLeaderboard() };
 }
 
-export default function Home({ loaderData }: Route.ComponentProps) {
+export default function Home({
+  loaderData: { leaderboard },
+}: Route.ComponentProps) {
   return (
     <main className="flex flex-col items-center gap-y-10 pt-10">
       <img
         src="/musylogo.png"
-        height={100}
-        width={100}
+        height={200}
+        width={200}
         draggable={false}
         className="select-none"
         alt="musy"
       />
 
-      <ul className="flex w-full max-w-sm flex-col gap-y-2">
-        {loaderData.leaderboard.map((song, index) => (
+      <ul className="flex w-full max-w-md flex-col gap-y-2">
+        {leaderboard.map((song, index) => (
           <li key={song.name}>
             <a href={song.uri} className="group flex items-center gap-x-2">
               <span className="basis-6">{index + 1}.</span>
               <div className="flex flex-1 gap-x-2 rounded bg-neutral-950 p-3 transition-colors duration-150 group-hover:bg-neutral-800">
-                <img src={song.image} alt={song.name} height={50} width={50} />
+                <img
+                  src={song.image}
+                  alt={song.name}
+                  height={80}
+                  width={80}
+                  className="self-start rounded"
+                />
                 <div>
-                  <p>{song.name}</p>
-                  <p className="text-neutral-500 text-sm">{song.artist}</p>
+                  <p className="line-clamp-2 text-ellipsis">{song.name}</p>
+                  <p className="line-clamp-1 text-ellipsis text-neutral-500 text-sm">
+                    {song.artist}
+                  </p>
                 </div>
               </div>
             </a>
