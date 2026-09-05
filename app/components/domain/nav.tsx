da90a4d0ea81d95b0d8e58b30de2766af65cd93f
Laurent <laurent.lucian@gmail.com>
2026-09-04 18:15:52 -0700

    style: add page and navbar profile padding

--- app/components/domain/nav.tsx
+++ app/components/domain/nav.tsx
@@ -64,7 +64,7 @@ export function Nav({
           </SidebarLink>
           <div className="mt-auto pt-10">
             {profile && (
-              <div className="mb-2 flex items-center gap-2 px-2">
+              <div className="mb-2 flex items-center gap-2 px-3 py-2">
                 <Link
                   to="/profile"
                   className="flex min-w-0 flex-1 items-center gap-2 py-2"
